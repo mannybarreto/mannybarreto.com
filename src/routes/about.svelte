75fd3ff8f53587e3222bb3009d7ac9fb6836492f
Manny Barreto <ma.barre98@gmail.com>
2021-06-30 15:44:59 -0700

    Initialized base style, added content to `about.svelte` and made it more responsive.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,12 +1,19 @@
-<div class="self-center m-auto w-1/4 grid grid-cols-3 gap-6">
-    <img src={"assets/profile/manny-circle.png"} alt={"Illustrated portrait of Manny"} class="" />
-    <div class="col-span-2 divide-y divide-green divide-opacity-75">
-        <div class="text-green mt-4">
+<div class="self-center m-auto w-1/2 xl:w-1/3 grid grid-cols-3 gap-6">
+    <img src={"assets/profile/manny-circle.png"} alt={"Illustrated portrait of Manny"} class="self-center m-auto" />
+    <div class="col-span-2 divide-y divide-yellow divide-opacity-60 self-center">
+        <div class="text-yellow">
             <div class="text-xl font-bold">Manny Barreto</div>
-            <div class="mb-2">Software Engineer @ <a href={'http://www.google.com'}>Google</a></div>
+            <div class="mb-2">Software Engineer @ <a href={'http://www.google.com'}>Google</a>, working on <a href={"https://apps.apple.com/us/app/google-maps/id585027354#?platform=iphone"}>iOS Maps</a></div>
         </div>
-        <div>
-            <div class="mt-1">Working on <a href={"https://apps.apple.com/us/app/google-maps/id585027354#?platform=iphone"}>iOS Google Maps</a></div>
-        </div>
+        <ul class="">
+            <li class="mt-1">Hello! I'm passionate about writing code as a craft, and honing the skills and arsenal involved. I hope to share that journey along with my various other passions on here.</li>
+            <li>Besides software engineering, some of my hobbies include: cooking, guitar, concert-going, hiking, travelling, and reading.</li>
+        </ul>
     </div>
 </div>
+
+<style lang="postcss">
+    li {
+        @apply ml-5
+    }
+</style>
