6d194d3989331a9c84d3b5352bb197bd774a946b
Manny Barreto <ma.barre98@gmail.com>
2021-09-25 15:13:55 -0700

    Change contact reference ot Neal Stephenson.

--- src/routes/contact/index.svelte
+++ src/routes/contact/index.svelte
@@ -1,61 +1,65 @@
 <script context="module" lang="ts">
-    // @type {import('@svelte/kit').Load}
-    export async function load({page, fetch}) {
-        const modules = import.meta.glob('./**.md')
+	// @type {import('@svelte/kit').Load}
+	export async function load({ page, fetch }) {
+		const modules = import.meta.glob('./**.md');
 
-        let socials = []
+		let socials = [];
 
-        for (const slug in modules) {
-            socials.push(modules[slug]().then(({metadata}) => metadata))
-        }
+		for (const slug in modules) {
+			socials.push(modules[slug]().then(({ metadata }) => metadata));
+		}
 
-        socials = await Promise.all(socials)
+		socials = await Promise.all(socials);
 
-        return {
-            props: {
-                socials: socials
-            }
-        }
-    }
+		return {
+			props: {
+				socials: socials
+			}
+		};
+	}
 </script>
 
 <script lang="ts">
-    import {SITE_POSTFIX, CONTACT_EMAIL} from "$lib/utils/constants";
-    import type {SocialFM} from "$lib/types/markdown";
+	import { SITE_POSTFIX, CONTACT_EMAIL } from '$lib/utils/constants';
+	import type { SocialFM } from '$lib/types/markdown';
 
-    export let socials: SocialFM[]
+	export let socials: SocialFM[];
 </script>
 
 <svelte:head>
-    <title>contact {SITE_POSTFIX}</title>
+	<title>contact {SITE_POSTFIX}</title>
 </svelte:head>
 
 <div class="divided divide-y divide-yellow divide-opacity-60 m-auto">
-    <h2 class="text-yellow">Contact Me</h2>
-    <div>
-        <p class="w-full my-3">
-            My career, family/friends, hobbies, and projects demand quite a lot of attention from my embarrassingly easily distracted mind.
-            Thus, after reading <a href="https://www.calnewport.com/contact/">Cal Newport's</a> philosophy on the matter,
-            I decided to quit social media with few exceptions (see below), and keep my correspondence to near essentials.
-            However, I can't help but want to hear from readers and colleagues, so please feel free to <a href={`mailto:${CONTACT_EMAIL}`}>reach out</a> if you deem it appropriate.
-        </p>
-    </div>
-    <div class="flex">
-        <h3 class="mt-auto">Socials: </h3>
-        {#each socials as social}
-            <a href={social.url} class="m-3 icon">
-                <img src={social.icon} alt={`${social.name} Icon`}/>
-            </a>
-        {/each}
-    </div>
+	<h2 class="text-yellow">Contact Me</h2>
+	<div>
+		<p class="w-full my-3">
+			My career, family/friends, hobbies, and projects demand quite a lot of attention from my
+			embarrassingly easily distracted mind. Thus, after reading Neal Stephenson's <a
+				href="https://www.nealstephenson.com/why-i-am-a-bad-correspondent.html"
+				>"Why I'm a Bad Correspondent"</a
+			>
+			philosophy on the matter, I decided to quit social media with few exceptions (see below), and keep
+			my correspondence to near essentials. However, I can't help but want to hear from readers and colleagues,
+			so please feel free to <a href={`mailto:${CONTACT_EMAIL}`}>reach out</a> if you deem it appropriate.
+		</p>
+	</div>
+	<div class="flex">
+		<h3 class="mt-auto">Socials:</h3>
+		{#each socials as social}
+			<a href={social.url} class="m-3 icon">
+				<img src={social.icon} alt={`${social.name} Icon`} />
+			</a>
+		{/each}
+	</div>
 </div>
 
 <style lang="postcss">
-    .icon {
-        /** Undo `a` hover effect from app.postcss */
-        &:hover,
-        &:focus {
-            @apply bg-background;
-        }
-    }
+	.icon {
+		/** Undo `a` hover effect from app.postcss */
+		&:hover,
+		&:focus {
+			@apply bg-background;
+		}
+	}
 </style>
--- src/routes/garden/index.svelte
+++ src/routes/garden/index.svelte
@@ -1,48 +1,51 @@
 <script context="module" lang="ts">
-    // @type {import('@svelte/kit').Load}
+	// @type {import('@svelte/kit').Load}
 
-    export async function load({page, fetch}) {
-        const modules = import.meta.glob('./**.md')
+	export async function load({ page, fetch }) {
+		const modules = import.meta.glob('./**.md');
 
-        let leaves = []
+		let leaves = [];
 
-        for (const slug in modules) {
-            leaves.push(modules[slug]().then(({metadata}) => {
-                return {...metadata, slug: slug}
-            }))
-        }
+		for (const slug in modules) {
+			leaves.push(
+				modules[slug]().then(({ metadata }) => {
+					return { ...metadata, slug: slug };
+				})
+			);
+		}
 
-        leaves = await Promise.all(leaves)
+		leaves = await Promise.all(leaves);
 
-        return {
-            props: {
-                leaves: leaves,
-            }
-        }
-    }
+		return {
+			props: {
+				leaves: leaves
+			}
+		};
+	}
 </script>
 
 <script lang="ts">
-    import {SITE_POSTFIX} from "$lib/utils/constants"
-    import type {Leaf} from "$lib/types/garden";
+	import { SITE_POSTFIX } from '$lib/utils/constants';
+	import type { Leaf } from '$lib/types/garden';
 
-    export let leaves: Leaf[]
+	export let leaves: Leaf[];
 </script>
 
 <svelte:head>
-    <title>garden {SITE_POSTFIX}</title>
+	<title>garden {SITE_POSTFIX}</title>
 </svelte:head>
 
 <div>
-    <h2 class="text-yellow">Welcome to my digital garden</h2>
-    <p>A loosely tended mess of my ideas, writings, and creations -- where I'll often find myself cultivating my passions.</p>
+	<h2 class="text-yellow">Welcome to my digital garden</h2>
+	<p>
+		A loosely tended mess of my ideas, writings, and creations -- where I'll often find myself
+		cultivating my passions.
+	</p>
 
-    <hr />
+	<hr />
 
-    <h2>
-        All Leaves
-    </h2>
-    {#each leaves as {slug}}
-        <a href="garden/{slug}" class="font-mono">{slug}</a>
-    {/each}
+	<h2>All Leaves</h2>
+	{#each leaves as { slug }}
+		<a href="garden/{slug}" class="font-mono">{slug}</a>
+	{/each}
 </div>
